99ccb956e0e0658833ba3e54de351e387c75decc
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:18:39 +0500

    count nummber of digit

--- main.dart
+++ main.dart
@@ -1,10 +1,18 @@
 void main() {
-  int number = 5;
-  int factorial = 1;
-  int i = 1;
-  while (i <= number) {
-    factorial = factorial * i;
-    i++;
+  int number = 99;
+  int count = 0;
+
+  if (number == 0) {
+    count = 1;
+  } else {
+    if (number < 0) {
+      number = -number;
+    }
+
+    while (number > 0) {
+      number = number ~/ 10;
+      count++;
+    }
+    print("THE COUNT IS $count");
   }
-  print("THE FACTORIAL OF $number is $factorial");
 }
