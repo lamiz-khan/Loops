f8045006228f298dd7cc9a483a5018963f33a1ec
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:20:21 +0500

    Check String is Empty or not

--- main.dart
+++ main.dart
@@ -1,27 +1,8 @@
-import 'dart:math';
-import 'dart:io';
-
 void main() {
-  stdout.write('Enter password length: ');
-  int? length = int.tryParse(stdin.readLineSync()!);
-
-  if (length == null || length <= 0) {
-    print('Invalid input. Please enter a positive number.');
-    return;
+  String name = "";
+  if (name.isEmpty) {
+    print("THE STRING IS EMPTY");
+  } else {
+    print("THE STRING IS NOT EMPTY");
   }
-
-  const String chars =
-      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%^&*()_+-=';
-  Random random = Random.secure();
-
-  String password = '';
-  int i = 0;
-
-  while (i < length) {
-    int index = random.nextInt(chars.length); // Pick a random index
-    password += chars[index]; // Add the character at that index
-    i++;
-  }
-
-  print('Generated Password: $password');
 }
