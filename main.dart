7c7afb6deeaf60debb7e825a29970130b584f9c2
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:26:24 +0500

    Find LArgest number in list

--- main.dart
+++ main.dart
@@ -1,7 +1,13 @@
 void main() {
-  int i = 1;
-  do {
-    print("Odd NUMBERS IN 1 TO 20 IS $i");
-    i += 2;
-  } while (i <= 20);
+  List<int> numbers = [55,8,245,65,99,200,10,456];
+
+  int largest = numbers[0]; // Assume the first number is the largest initially
+
+  for (int i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+
+  print('The largest number is $largest');
 }
