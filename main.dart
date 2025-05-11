5af08480b9180dafe8171852039d97de32b76c16
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:22:13 +0500

    First Ten Natural Numbers

--- main.dart
+++ main.dart
@@ -1,24 +1,8 @@
 void main() {
-  int count = 0;
-  int number = 2;
-
-  print("First 10 prime numbers:");
-
-  while (count < 10) {
-    bool isPrime = true;
-
-    for (int i = 2; i <= number ~/ 2; i++) {
-      if (number % i == 0) {
-        isPrime = false;
-        break;
-      }
-    }
-
-    if (isPrime) {
-      print(number);
-      count++;
-    }
-
-    number++;
+  int num = 1;
+  int i = 10;
+  while (i >= num) {
+    print("THE FIRST TEN NATURAL NUMBER =  $i");
+    i--;
   }
 }
