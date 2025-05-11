f32ef7ed438e0add1eb5a8698050ae01383d6fe1
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:21:24 +0500

    Ten Prime Numbers

--- main.dart
+++ main.dart
@@ -1,8 +1,24 @@
 void main() {
-  String name = "";
-  if (name.isEmpty) {
-    print("THE STRING IS EMPTY");
-  } else {
-    print("THE STRING IS NOT EMPTY");
+  int count = 0;
+  int number = 2;
+
+  print("First 10 prime numbers:");
+
+  while (count < 10) {
+    bool isPrime = true;
+
+    for (int i = 2; i <= number ~/ 2; i++) {
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      print(number);
+      count++;
+    }
+
+    number++;
   }
 }
