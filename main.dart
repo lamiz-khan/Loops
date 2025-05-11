4f0edfbf75b848efadbd08fb3eb727fcdb7f2303
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:17:57 +0500

    Factorial of given number

--- main.dart
+++ main.dart
@@ -1,10 +1,10 @@
 void main() {
-  int num1 = 55;
-  if (num1 > 0) {
-    print("THE NUMBER IS POSITIVE");
-  } else if (num1 < 0) {
-    print("THE NUMBER IS NEGATIVE");
-  } else {
-    print("THE NUMBER IS ZERO");
+  int number = 5;
+  int factorial = 1;
+  int i = 1;
+  while (i <= number) {
+    factorial = factorial * i;
+    i++;
   }
+  print("THE FACTORIAL OF $number is $factorial");
 }
