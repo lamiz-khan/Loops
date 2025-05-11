8a8430aae6fabb103f0b7417a2d829a6be5d5b22
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:19:21 +0500

    RAdnom Password Generated

--- main.dart
+++ main.dart
@@ -1,18 +1,27 @@
+import 'dart:math';
+import 'dart:io';
+
 void main() {
-  int number = 99;
-  int count = 0;
+  stdout.write('Enter password length: ');
+  int? length = int.tryParse(stdin.readLineSync()!);
 
-  if (number == 0) {
-    count = 1;
-  } else {
-    if (number < 0) {
-      number = -number;
-    }
-
-    while (number > 0) {
-      number = number ~/ 10;
-      count++;
-    }
-    print("THE COUNT IS $count");
+  if (length == null || length <= 0) {
+    print('Invalid input. Please enter a positive number.');
+    return;
   }
+
+  const String chars =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%^&*()_+-=';
+  Random random = Random.secure();
+
+  String password = '';
+  int i = 0;
+
+  while (i < length) {
+    int index = random.nextInt(chars.length); // Pick a random index
+    password += chars[index]; // Add the character at that index
+    i++;
+  }
+
+  print('Generated Password: $password');
 }
