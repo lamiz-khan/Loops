ca52fcb18235f9fb38b4a1106909660d987291e6
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:31:20 +0500

    Number traingle

--- main.dart
+++ main.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
-
 void main() {
-  stdout.write("PLEASE ENTER THE NUMBER: ");
-  int? number = int.parse(stdin.readLineSync()!);
-  for (int i = 1; i<=10; i++){
-    print ("$number * $i = ${number * i} ");
+  int rows = 5;
+  for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write("$i");
+    }
+    print("");
   }
 }
