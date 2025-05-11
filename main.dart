6f8463c2387eeead90d10fa1ed5bd2d4daa41bd5
lamiz-khan <khanlamiz8@gmail.com>
2025-05-11 13:28:34 +0500

    Multiplication of given number

--- main.dart
+++ main.dart
@@ -1,13 +1,9 @@
+import 'dart:io';
+
 void main() {
-  List<int> numbers = [55,8,245,65,99,200,10,456];
-
-  int largest = numbers[0]; // Assume the first number is the largest initially
-
-  for (int i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-      largest = numbers[i];
-    }
+  stdout.write("PLEASE ENTER THE NUMBER: ");
+  int? number = int.parse(stdin.readLineSync()!);
+  for (int i = 1; i<=10; i++){
+    print ("$number * $i = ${number * i} ");
   }
-
-  print('The largest number is $largest');
 }
